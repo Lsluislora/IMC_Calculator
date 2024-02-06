9fd6f306cfda8175d92a133be1546d43267c32d5
lalora <lsluislora01@gmail.com>
2024-02-06 21:18:56 +0100

    include news funtions!

--- app/src/main/java/com/project/imccalculater/MainActivity.kt
+++ app/src/main/java/com/project/imccalculater/MainActivity.kt
@@ -1,16 +1,76 @@
 package com.project.imccalculater
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var isMaleSelected = true
+    private var isFemaleSelected = false
+
+    private lateinit var cardviewFemale: CardView
+    private lateinit var cardviewMale: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent(this, MainActivity::class.java)
-
+        initComponent()
+        initListener()
+        initUI()
 
     }
+
+    private fun initUI() {
+        setGenderColor()
+    }
+
+
+    private fun initComponent() {
+        cardviewFemale = findViewById(R.id.cardviewFemale)
+        cardviewMale = findViewById(R.id.cardviewMale)
+
+    }
+
+    private fun initListener() {
+
+        cardviewFemale.setOnClickListener {
+
+            changeGenderColor()
+            setGenderColor()
+        }
+
+        cardviewMale.setOnClickListener {
+            changeGenderColor()
+            setGenderColor()
+        }
+
+    }
+
+    private fun setGenderColor() {
+
+        cardviewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected));
+        cardviewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelected));
+
+    }
+
+    private fun changeGenderColor() {
+        isMaleSelected = !isMaleSelected
+        isFemaleSelected = !isFemaleSelected
+    }
+
+    private fun getBackgroundColor(isSelectectComponent: Boolean): Int {
+
+        val referenceColor = if (isSelectectComponent) {
+            R.color.selected_background_cardview
+        } else {
+            R.color.background_cardview
+        }
+
+        return ContextCompat.getColor(this, referenceColor)
+
+    }
+
 }
